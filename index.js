2522b1fb5c227df52ed7639484b961f78e0a99dd
Anthony <anthonyjlower@gmail.com>
2018-01-07 17:13:40 -0600

    Logs the information about the car

---
+++ index.js
@@ -0,0 +1,14 @@
+const car = require('./Car.js');
+
+
+console.log("The car is ", car.color);
+if (car.convertible) {
+	console.log("The car is a convertible")
+} else {
+	console.log("The car is not a convertible")
+};
+
+console.log("the current speed is ", car.speed);
+
+car.accelerate(car.speed);
+car.decelerate(car.speed);
