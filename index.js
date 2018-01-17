1b5b56489145cf08c892e15afd7efe2c662586de
Michael Weber <webermn15@gmail.com>
2018-01-16 20:08:39 -0600

    it's all connected and working

---
+++ index.js
@@ -0,0 +1,7 @@
+const module = require('./car.js');
+
+console.log(module.color);
+console.log(module.convertible);
+console.log(module.speed);
+console.log(module.accelerateCar);
+console.log(module.decelerateCar);
